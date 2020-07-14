e64a43af69055528c05290ba4542e580f1764cc9
Look_YYJ_VBUbuntu <sockyjam@126.com>
2020-07-14 18:09:30 +0800

    x

--- run_init.sh
+++ run_init.sh
@@ -1,5 +1,9 @@
 docker run -d -p 27017:27017 -v $PWD/dbdata:/data/db  --name mongo-yapi mongo
 
+docker run -it --rm --link mongo-yapi:mongo --entrypoint npm --workdir /api/vendors \
+	  registry.cn-hangzhou.aliyuncs.com/anoy/yapi \
+	    run install-server
+
 
 docker run -d \
 	  --name yapi \
