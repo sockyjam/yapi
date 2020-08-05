676d2c977d0666446e7d29799b02110d9f667c1c
root@lookstore <rootlookstore@loookauto.com>
2020-08-05 14:06:54 +0800

    only bakup db json

--- bakup.sh
+++ bakup.sh
@@ -1,9 +1,12 @@
 set -x;
 time=`date +%Y%m%d`
-BAK_DIR=/root/yapi
+BAK_DIR=/root/0bak/yapi
 
-tar zcvf $BAK_DIR/dbdata_$time.tar.gz -C $BAK_DIR dbdata/
+
+$BAK_DIR/dump_mongo.sh yapi $BAK_DIR/db
+
+tar zcvf $BAK_DIR/dbdata_$time.tar.gz -C $BAK_DIR db/
 
 find $bakdir -name "*.tar.gz" -type f -mtime +100 -exec rm -rf {} \; > /dev/null 2>&1
-
+rm -rf $BAK_DIR/db/
 
